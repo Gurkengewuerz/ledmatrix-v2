0bd6d541294c2e4cdd8830b27053593909d8bb01
Gurkengewuerz <niklas@schuetrumpf.net>
2023-04-11 00:08:58 +0200

    feat: scroll row wise not col wise

--- lib/LEDMatrix/AnimationGradient.cpp
+++ lib/LEDMatrix/AnimationGradient.cpp
@@ -5,9 +5,9 @@ bool AnimationGradient::update() {
 
     const uint16_t colorStep = (uint16_t)((65535 / 100) * 0.5f);
 
-    for (int16_t col = 1; col <= this->display->getCols(); col++) {
-        uint32_t color = display->getColorHSV((this->colorPercentage + ((col - 1) * colorStep)) % 65535, this->saturation, 255);
-        for (int16_t row = 1; row <= this->display->getRows(); row++) {
+    for (int16_t row = 1; row <= this->display->getRows(); row++) {
+        uint32_t color = display->getColorHSV((this->colorPercentage + ((row - 1) * colorStep)) % 65535, this->saturation, 255);
+        for (int16_t col = 1; col <= this->display->getCols(); col++) {
             this->display->setPixel(col, row, color, false);
         }
     }
